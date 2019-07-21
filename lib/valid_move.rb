c11c9745083c5832b524a9de0f68e4c607eb4c2d
Micah Levine <micah.levine@outlook.com>
2019-07-21 19:00:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,23 +1,20 @@
 # code your #valid_move? method here
 
-def input_to_index(user_input)
-  user_input.to_i -1
-end
-
-def valid_move? (input)
-  if input >8 then false
-  
+def valid_move?(board, index)
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+       true
     end
-  end
+ end
+
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
 end
 
 
 
 
-def position_taken? (board, index)
-  if  board[index] == "" || board[index] == " " || board[index] == nil
-    false
-  else
-    true
-  end
-end
+
